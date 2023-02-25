a24699bc162afc94ec79cae5fc25a1516093d88f
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-02-25 20:10:45 -0300

    Atualizado

--- controller/usuario.controller.js
+++ controller/usuario.controller.js
@@ -21,7 +21,18 @@ const usuarios = [
 
 const find = (req, res) => {
     const id = req.params.id;
-    res.send(usuarios[id]);
+    let found = false;
+
+    usuarios.map(function (valor) {
+        if (valor.id == id) {
+            found = true;
+            return res.send(valor);
+        }
+    });
+
+    if (!found) {
+        res.status(404).send({ message: `O usuário com o id ${id} não foi encontrado.` });
+    }
 }
 
 const findAllUsuarios = (req, res) => {
@@ -30,15 +41,105 @@ const findAllUsuarios = (req, res) => {
 
 const createUsuario = (req, res) => {
     const usuario = req.body;
-    if (req.body.nome == null) {
-        return res.send({ message: "Corpo da mensagem está vazio." });
+    let found = false;
+
+    if (Object.keys(usuario).length === 0) {
+        return res.status(400).send({ message: "O corpo da mensagem está vazio." });
     }
+
+    if (!usuario.id) {
+        return res.status(400).send({ message: "O campo 'id' não foi informado." });
+    }
+
+    if (usuario.id) {
+        usuarios.map(function (valor) {
+            if (valor.id == usuario.id) {
+                return found = true;
+            }
+        });
+    }
+
+    if (found) {
+        res.status(400).send({ message: `O usuário com o id ${usuario.id} já consta em nossa base de dados.` });
+    }
+
+    if (!usuario.nome) {
+        return res.status(400).send({ message: "O campo 'nome' não foi informado." });
+    }
+
+    if (!usuario.sobrenome) {
+        return res.status(400).send({ message: "O campo 'sobrenome' não foi informado." });
+    }
+
+    if (!usuario.idade) {
+        return res.status(400).send({ message: "O campo 'idade' não foi informado." });
+    }
+
+
+    usuario.statusSistema = "ativo";
     usuarios.push(usuario);
-    res.send(usuarios);
+    res.status(201).send(usuarios);
+}
+
+const updateUsuario = (req, res) => {
+    const id = req.params.id;
+    const usuario = req.body;
+    let found = false;
+
+    if (Object.keys(usuario).length === 0) {
+        return res.status(400).send({ message: "O corpo da mensagem está vazio." });
+    }
+
+    if (!usuario.id) {
+        return res.status(400).send({ message: "O campo 'id' não foi informado." });
+    }
+
+    if (!usuario.nome) {
+        return res.status(400).send({ message: "O campo 'nome' não foi informado." });
+    }
+
+    if (!usuario.sobrenome) {
+        return res.status(400).send({ message: "O campo 'sobrenome' não foi informado." });
+    }
+
+    if (!usuario.idade) {
+        return res.status(400).send({ message: "O campo 'idade' não foi informado." });
+    }
+
+    usuarios.map(function (valor, index) {
+        if (valor.id == id) {
+            found = true;
+            usuarios[index] = usuario;
+            return res.send(usuarios[index]);
+        }
+    });
+
+    if (!found) {
+        res.status(404).send({ message: `O usuário com o id ${id} não foi encontrado.` });
+    }
+}
+
+const deleteUsuario = (req, res) => {
+    const id = req.params.id;
+    let found = false;
+
+    usuarios.map(function (valor, index) {
+        if (valor.id == id) {
+            found = true;
+            usuarios.splice(index, 1);
+            return res.send(valor);
+        }
+    });
+
+    if (!found) {
+        res.status(404).send({ message: `O usuário com o id ${id} não foi encontrado.` });
+    }
 }
 
 module.exports = {
     find,
     findAllUsuarios,
-    createUsuario
+    createUsuario,
+    updateUsuario,
+    deleteUsuario
 }
--- router/usuario.router.js
+++ router/usuario.router.js
@@ -4,5 +4,7 @@ const usuario = require('../controller/usuario.controller');
 router.get('/find/:id', usuario.find);
 router.get('/findAll', usuario.findAllUsuarios);
 router.post('/create', usuario.createUsuario);
+router.put('/update/:id', usuario.updateUsuario);
+router.delete('/delete/:id', usuario.deleteUsuario);
 
 module.exports = router;
